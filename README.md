2d6b144f63040c43ed775ad2fec61e5f18593c54
Sho Takahashi　 <sho.takahashi@yts-inc.net>
2020-10-08 05:02:24 +0900

    Create config.json to extract authentication information.

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # GoOAuth
 
-GoでOAuthを行うサンプルプログラム
+## 概要
+GoでOAuthを行うサンプルプログラム  
+
+## セットアップ
+
+`go get github.com/markbates/goth`  
+を実行して、 `goth` モジュールを取得。
+
+`config.json`に以下のように、各プラットフォームの `client id`や`secret` を記述していく必要がある。
+
+```config.json
+{
+    "google":{
+        "client_id" : "YOUR_CLIENT_ID",
+        "secret" : "YOUR_SECRET"
+    }
+    "amazon":{
+        "client_id" : "YOUR_CLIENT_ID",
+        "secret" : "YOUR_SECRET"
+    }
+}
+```
--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
   "fmt"
   "html/template"
   "net/http"
+
+  "encoding/json"
+  "io/ioutil"
+  "os"
   
   "log"
 
@@ -11,51 +15,73 @@ import (
   "github.com/markbates/goth"
   "github.com/markbates/goth/gothic"
   "github.com/markbates/goth/providers/google"
-  "github.com/gorilla/sessions"
+  "github.com/markbates/goth/providers/amazon"
 )
 
+var port = ":3000"
+var hostName = "http://localhost" + port
+
+type Config struct{
+  Google Provider `json:"google"`
+  Amazon Provider `json:"amazon"`
+}
+
+type Provider struct{
+  ClientID string `json:"client_id"`
+  Secret string `json:"secret"`
+  Callback string `json:"callback"`
+}
+
+func readConfig(config *Config)  {
+  raw, err := ioutil.ReadFile("./config.json")
+  if err != nil {
+      fmt.Println(err.Error())
+      os.Exit(1)
+  }
+
+  json.Unmarshal(raw, &config)  
+}
+
+func indexHandlerfunc(res http.ResponseWriter, req *http.Request) {
+  t, _ := template.ParseFiles("templates/index.html")
+  t.Execute(res, false)
+}
+
+func authHandler(res http.ResponseWriter, req *http.Request) {
+  var config Config
+  readConfig(&config)
+
+  provider := req.URL.Query().Get(":provider")
+  switch provider {
+    case "google":
+      goth.UseProviders(
+        google.New(config.Google.ClientID, config.Google.Secret, hostName+ "/auth/google/callback", "email", "profile"),
+      )
+    case "amazon":
+      goth.UseProviders(
+        amazon.New(config.Amazon.ClientID, config.Amazon.Secret, hostName+ "/auth/amazon/callback", "profile", "profile:user_id"),
+      ) 
+
+  }
+  gothic.BeginAuthHandler(res, req)
+}
+
+func callBackHandler(res http.ResponseWriter, req *http.Request) {
+  user, err := gothic.CompleteUserAuth(res, req)
+  if err != nil {
+    fmt.Fprintln(res, err)
+    return
+  }
+  t, _ := template.ParseFiles("templates/success.html")
+  t.Execute(res, user)
+}
 
 func main() {
-  
-  key := "session_secret"  // Replace with your SESSION_SECRET or similar
-  maxAge := 86400 * 30  // 30 days
-  isProd := false       // Set to true when serving over https
-
-  store := sessions.NewCookieStore([]byte(key))
-  store.MaxAge(maxAge)
-  store.Options.Path = "/"
-  store.Options.HttpOnly = true   // HttpOnly should always be enabled
-  store.Options.Secure = isProd
-
-  gothic.Store = store
-
-  clientID := "Your client id"
-  secret := "Your secret"
-
-  goth.UseProviders(
-    google.New(clientID, secret, "http://localhost:3000/auth/google/callback", "email", "profile"),
-  )
-
   p := pat.New()
-  p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+  p.Get("/auth/{provider}/callback", callBackHandler)
+  p.Get("/auth/{provider}", authHandler)
+  p.Get("/", indexHandlerfunc)
 
-    user, err := gothic.CompleteUserAuth(res, req)
-    if err != nil {
-      fmt.Fprintln(res, err)
-      return
-    }
-    t, _ := template.ParseFiles("templates/success.html")
-    t.Execute(res, user)
-  })
-
-  p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
-    gothic.BeginAuthHandler(res, req)
-  })
-
-  p.Get("/", func(res http.ResponseWriter, req *http.Request) {
-    t, _ := template.ParseFiles("templates/index.html")
-    t.Execute(res, false)
-  })
-  log.Println("listening on localhost:3000")
-  log.Fatal(http.ListenAndServe(":3000", p))
+  log.Println("listening on " + hostName)
+  log.Fatal(http.ListenAndServe(port, p))
 }
